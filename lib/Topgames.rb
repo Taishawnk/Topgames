62bfda3291e55795efea2139f2353916d70fae5f
Taishawnk <Taishawnking1@yahoo.com>
2020-05-11 13:33:09 -0600

    redo

--- lib/Topgames.rb
+++ lib/Topgames.rb
@@ -3,4 +3,11 @@ require  "pry"
 require  "open-uri"
 require_relative "Topgames/version"
 require_relative './Topgames/cli'
-require_relative './Topgames/gamex'
+ require_relative './Topgames/gamex'
+require_relative './Topgames/scrapper'
+
+
+
+
+
+
